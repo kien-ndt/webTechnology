ae5fe649112671eb26e1379ac569892a1ae69647
Trung Kien <nightstealth99@gmail.com>
2020-12-20 17:15:13 +0700

    a

--- app/views/single/admin/adminSidebar.php
+++ app/views/single/admin/adminSidebar.php
@@ -1,8 +1,13 @@
-<div class="sidebar">
-    <ul>
-        <li><a href="<?php echo BASE_URL?>">Trang chủ</a></li>
-        <li><a href="<?php echo BASE_URL?>admin/product">Quản lý sản phẩm</a></li>
-        <li><a href="<?php echo BASE_URL?>admin/addcategory">Quản lý danh mục</a></li>
-        
+<div class="menu" >
+    <ul style="padding: 0px; margin: 0px;">
+        <li class="item">
+            <a href="" class="btn">Admin</a>
+            <div class="smenu">
+                <a href="<?php echo BASE_URL?>">Trang chủ</a>
+                <a href="<?php echo BASE_URL?>admin/product">Quản lý sản phẩm</a>
+                <a href="<?php echo BASE_URL?>admin/addcategory">Quản lý danh mục</a>
+                <a href="<?php echo BASE_URL?>admin/signout">Đăng xuất</a>
+            </div>
+        </li>
     </ul>
 </div>
